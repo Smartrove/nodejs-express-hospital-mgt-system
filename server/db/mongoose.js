1d92c4e8aaefc666550e26bb00cd1a4380cabd58
Lateef Wasiu Olalekan <olaokunolalekan@gmail.com>
2022-11-03 07:44:26 +0100

    first commit

---
+++ server/db/mongoose.js
@@ -0,0 +1,8 @@
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv/config");
+
+// mongoose.Promise = global.Promise;
+
+// //change the database with yours
+
+// module.exports = connect;
